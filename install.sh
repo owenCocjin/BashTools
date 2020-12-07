28e06e066b48f7f5e9849d43def9d137f2c616e8
Owen Cocjin <owen_jamescf@hotmail.com>
2020-12-07 00:34:22 -0500

    Added install.sh

---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+## Author:	Owen Cocjin
+## Version:	1.0
+## Date:    07/12/20
+## Title:   install.sh
+## Description: Installs BashTools
+## Notes:
+##    - Must run as root
+#Exit if not running as root
+if [[ $(id -u) != '0' ]]; then
+	echo "[|X] Run as root (will probably fail otherwise)!"
+	exit 1
+fi
+
+#Create temp var for bashtools home directory
+temp_path='/usr/local/bin'
+echo "[|X] Deciding home for bashtools... ${temp_path}"
+
+#Sym link Tools to path
+ln -s ${PWD}/Tools ${temp_path}/bashtools
+echo -e "[|X] Creating symlink from\n\t-> ${temp_path}/bashtools to ${PWD}/Tools ..."
+
+#Add lines to .bashrc
+echo -e "\nexport BASHTOOLS_PATH=${temp_path}
+. ${temp_path}/bashtools/.sources" >> /home/${SUDO_USER}/.bashrc
+echo "[|X] Adding lines to /home/${SUDO_USER}/.bashrc ..."
+
+#Tell user to source bash to take effect
+echo "[|X] All done! Source your bashrc file to get started!
+
+. ~/.bashrc
+"
