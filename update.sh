011111bb09de78e4091d0ab6dc3f38601b910258
Owen Cocjin <owen_jamescf@hotmail.com>
2020-12-10 23:59:38 -0500

    Fixed typos and made prettier

--- update.sh
+++ update.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 ## Author:	Owen Cocjin
-## Version:	1.2
+## Version:	1.3
 ## Date:    06/12/20
 ## Title:   update.sh
 ## Description: Updates all bashTools
 ## Notes:
-##    - Requires 'wget' program
-##    - Imcorporated env var 'BASHTOOLS_PATH'
+##    - Requires 'wget'
+##    - Fixed typos and spacing
 
 echo -e "This script will be modifying files in:\n\t${BASHTOOLS_PATH}\nIf this is not the correct directory change env var 'BASHTOOLS_PATH'."
 echo -en "\nIs this the correct directory (y/n)? "
@@ -34,6 +34,7 @@ for f in $(ls ${BASHTOOLS_PATH}); do
 done
 
 #List updated versions
+echo "********************************"
 counter=0
 for f in $(ls ${BASHTOOLS_PATH}); do
 	newVersion=$(head -n 3 "${BASHTOOLS_PATH}/${f}" | tail -n 1)
@@ -41,7 +42,8 @@ for f in $(ls ${BASHTOOLS_PATH}); do
 	echo -en "\tVersion ${oldVersion[counter]:12} -> ${newVersion:12}\n"
 	((++counter))
 done
+echo "********************************"
 
 #Can't source bashrc from script
-echo "Done! finally, source bashrc to complete update!"
+echo -e "\nDone! Finally, source bashrc to complete update!"
 echo -e '\n. ~/.bashrc\n'
